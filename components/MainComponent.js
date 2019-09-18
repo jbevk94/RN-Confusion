1b80d4425b16e7da391d95bb15a01a812d19052a
Beverly James <jbevk94@gmail.com>
2019-09-18 16:56:15 -0700

    Navigation Part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
-import { DISHES } from '../shared/dishes';
 import { View, Platform } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
+
 
 const MenuNavigator = createStackNavigator({
     Menu: { screen: Menu },
@@ -21,28 +22,45 @@ const MenuNavigator = createStackNavigator({
     }
 });
 
+const HomeNavigator = createStackNavigator({
+    Home: { screen: Home }
+  }, {
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: "#512DA8"
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff"
+      }
+    }
+});
+
+const MainNavigator = createDrawerNavigator({
+    Home: {
+      screen: HomeNavigator,
+      navigationOptions: {
+        title: 'Home',
+        drawerLabel: 'Home'
+      }
+    },
+    Menu: {
+      screen: MenuNavigator,
+      navigationOptions: {
+        title: 'Menu',
+        drawerLabel: 'Menu'
+      }
+    }
+  }, {    drawerBackgroundColor: '#D1C4E9'});
+
 class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        dishes: DISHES,
-        selectedDish: null
-      };
-  }
-
-  onDishSelect(dishId) {
-    this.setState({selectedDish: dishId})
-}
-
   render() {
- 
-    return (
-       
-        <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
-            <MenuNavigator />
-        </View>
+    return(
+      <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
+        <MainNavigator />
+      </View>
     );
   }
 }
-  
+
 export default Main;
