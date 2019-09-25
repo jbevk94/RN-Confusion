eded059b83f92ccaaa56de8da320c6fe44c6870f
Beverly James <jbevk94@gmail.com>
2019-09-24 17:09:16 -0700

    Activity Indicator

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -3,6 +3,7 @@ import { View, FlatList } from "react-native";
 import { Tile } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import { Loading } from "./LoadingComponent";
 
 const mapStateToProps = state => {
   return {
@@ -11,7 +12,6 @@ const mapStateToProps = state => {
 };
 
 class Menu extends Component {
-  
   static navigationOptions = {
     title: "Menu"
   };
@@ -20,25 +20,36 @@ class Menu extends Component {
     const renderMenuItem = ({ item, index }) => {
       return (
         <Tile
-                    key={index}
-                    title={item.name}
-                    caption={item.description}
-                    featured
-                    onPress={() => navigate('DishDetail', { dishId: item.id })}
-                    imageSrc={{ uri: baseUrl + item.image}}
-                    />
+          key={index}
+          title={item.name}
+          caption={item.description}
+          featured
+          onPress={() => navigate("DishDetail", { dishId: item.id })}
+          imageSrc={{ uri: baseUrl + item.image }}
+        />
       );
     };
 
     const { navigate } = this.props.navigation;
 
-    return (
-        <FlatList 
-        data={this.props.dishes.dishes}
-        renderItem={renderMenuItem}
-        keyExtractor={item => item.id.toString()}
+    if (this.props.dishes.isLoading) {
+      return <Loading />;
+    } else if (this.props.dishes.errMess) {
+      return (
+        <View>
+          <Text>{props.dishes.errMess}</Text>
+        </View>
+      );
+    } else {
+      return (
+        <FlatList
+          data={this.props.dishes.dishes}
+          renderItem={renderMenuItem}
+          keyExtractor={item => item.id.toString()}
         />
-    );
+      );
+    }
+
   }
 }
 export default connect(mapStateToProps)(Menu);
