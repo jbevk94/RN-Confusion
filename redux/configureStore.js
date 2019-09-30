e23eb945b8fd49e33c8ef561525292d1e1d5543d
Beverly James <jbevk94@gmail.com>
2019-09-30 11:02:43 -0700

    Persist Redux Store

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -1,22 +1,34 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import logger from 'redux-logger';
 import { dishes } from "./dishes";
 import { comments } from "./comments";
 import { promotions } from "./promotions";
 import { leaders } from "./leaders";
 import { favorites } from "./favorites";
+import { persistStore, persistCombineReducers } from 'redux-persist';
+import storage from 'redux-persist/es/storage';
 
 export const ConfigureStore = () => {
-  const store = createStore(
-    combineReducers({
+
+const config = {
+  key: 'root',
+  storage,
+  debug: true
+};
+
+const store = createStore(
+  persistCombineReducers(config, {
       dishes,
       comments,
       promotions,
       leaders,
       favorites
-    }),
+  }),
     applyMiddleware(thunk)
   );
 
-  return store;
-};
+  const persistor = persistStore(store);
+
+  return { persistor, store };
+}
